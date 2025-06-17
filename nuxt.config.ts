755a495e5484e78985e42b091a6be226dee0b5d3
Bravco <boss.wiw@gmail.com>
2025-06-17 22:47:12 +0200

    Fixed the scroll overflow bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,10 @@ export default defineNuxtConfig({
           name: "description",
           content: "I am a creative web developer who turns ideas into visually captivating and functional websites. Solid and professional website development. Includes design, programming, hosting and setuping up domain!",
         },
+        {
+          name: "keywords",
+          content: "website development, web development, website developer, web developer, web design, website design, web designer, website desginer"
+        }
       ],
     },
   },
